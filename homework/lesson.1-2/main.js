b8436316830b0b5551c5ee225f4102734e238537
Roman Liutikov <roman01la@romanliutikov.com>
2016-07-17 00:58:08 +0300

    add homework for lessons 1-2

---
+++ homework/lesson.1-2/main.js
@@ -0,0 +1,31 @@
+json // JSON data is in `json` global variable
+
+/*
+ * 1. Fix and convert balance value to number
+ *
+ *    Produce a new array of users including fixed balance values
+ */
+var users = 'Fix me';
+
+
+/*
+ * 2. Group users by balance (1000 - 2000, 2000+)
+ *
+ *    Groups should be defined in a single object,
+ *    where key is the name of the group
+ *    and value is array of users
+ *    { '1000-2000': [], '2000+': [] }
+ */
+var grouped = 'Fix me';
+
+
+/*
+ * 3. Find average age and balance in all groups
+ *
+ *    The final result should be an object
+ *    where key is the name of the group
+ *    and value is an object of two fields:
+ *    average age and average balance
+ *    { '1000-2000': { avgAge: Number, avgBalance: Number }, ... }
+ */
+var averages = 'Fix me';
