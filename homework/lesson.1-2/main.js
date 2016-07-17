35ccf5bd04549877122b3c41900f4fd2e7a938c0
Roman Liutikov <roman01la@romanliutikov.com>
2016-07-17 13:27:26 +0300

    more info for 1st exercise

--- homework/lesson.1-2/main.js
+++ homework/lesson.1-2/main.js
@@ -4,6 +4,10 @@ json // JSON data is assigned to `json` global variable which is declared in `js
  * 1. Fix and convert balance value to number
  *
  *    Produce a new array of users including fixed balance values
+ *    `balance` values might be of three types:
+ *      - number as a string '100.5' (convert to Number type)
+ *      - number as a string + currency symbol '100.5$' (remove currency symbol and convert to Number)
+ *      - `false` (convert to 0)
  */
 var users = 'Fix me';
 
