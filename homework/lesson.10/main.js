a140453e869e5fae2325ed8f849d889261c95064
Roman Liutikov <roman01la@romanliutikov.com>
2016-08-02 20:50:38 +0300

    add homework for lesson 10

---
+++ homework/lesson.10/main.js
@@ -0,0 +1,20 @@
+/* === Exercise ===
+ *
+ * == Description ==
+ * Given existing markup develop drag'n'drop game
+ * where user should drag colored circle into gray circle
+ * and colored rectangle into gray rectangle.
+ * When colored shape is placed inside of corresponding
+ * gray shape, gray shape should be highlighted with a color.
+ *
+ * == Suggestions ==
+ * - Use `mousedown` and `mouseup` events on colored shape to determine
+ *   if it can be dragged.
+ * - Use `mousemove` event on colored shape to move it
+ *   when it's pressed and check if it's inside of corresponding
+ *   gray shape.
+ * - Use `getBoundingClientRect()` to get element's position coordinates.
+ *
+ * See expected result on video: https://youtu.be/oCynnKKqhq8
+ * If you can not solve this exercise, see result here: http://jsbin.com/hanusokadu/edit?js,output
+ */
